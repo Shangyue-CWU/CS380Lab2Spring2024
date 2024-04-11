0c0afe30eb06f30956d81ec97d60cd3a99c1e52c
Shangyue-CWU <144855209+Shangyue-CWU@users.noreply.github.com>
2024-04-11 13:11:37 -0700

    remove Test.java

--- Test.java
+++ Test.java
@@ -1,18 +1,1 @@
-/**
- * The Test class is used to test the Bobcat class and its methods.
- */
-public class Test {
-    public static void main(String[] args) {
-        // Create a Bobcat object with the name "Bobby".
-        Bobcat bobcat = new Bobcat("Bobby");
 
-        // Call the sound() method to make the Bobcat produce a sound.
-        bobcat.sound();
-
-        // Call the play() method to simulate the Bobcat's playful activity.
-        bobcat.play();
-
-        // Call the eat() method to indicate that the Bobcat is eating food.
-        bobcat.eat();
-    }
-}
