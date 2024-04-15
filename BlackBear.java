0749aff4a20e251fefc4c6dc5cd48511632725f1
HoweyLuong <hieuluong1311@gmail.com>
2024-04-15 13:37:55 -0700

    Added a new sound of the Black Bear

--- BlackBear.java
+++ BlackBear.java
@@ -1,7 +1,17 @@
-package Project2;
+
 
 public class BlackBear extends Animal {
+    /**
+     * A constructor which makes a Black Bear object
+     * 
+     * @param name the name of the Black Bear
+     */
 
+     public BlackBear (String name) {
+        super(name);
+     }
+
+     
 	
 
 	
