e059e63619f8f1bc8d2d6cbc1a382f98f7acb70d
HoweyLuong <hieuluong1311@gmail.com>
2024-04-15 13:59:18 -0700

    Added a function to test about the Black Bear

--- Test.java
+++ Test.java
@@ -5,7 +5,17 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
+			// Create a Black Bear object with the name "Jade".
+			BlackBear temp = new BlackBear ("Jade");
+
+
+			//Call the sound() method to make the Black Bear produce a sound
+			temp.sound();
+			//Call the play() method to make the simulate the Black Bear's playful activity.
+			temp.play();
+			//Call the eat() method to indicate that the Black Bear is eating food
+			temp.eat();
+	
 
 	}
 
