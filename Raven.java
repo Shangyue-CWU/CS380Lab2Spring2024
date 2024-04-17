604c19c10b58eb0e05bd2ce84160f3a1d4005301
Yonatan Yemiru <Yonatan.7683@gmail.com>
2024-04-17 16:00:24 -0700

    added new raven class

---
+++ Raven.java
@@ -0,0 +1,21 @@
+public class Raven extends Animal{
+
+    public Raven(String name) {
+        super(name);
+    }
+    
+    @Override
+public void sound(){
+    System.out.println(this.name + "quak");
+    
+    
+   
+    
+    
+}
+    @Override
+ public void play(){
+    System.out.println(this.name +"raven flies");
+ }
+  
+}
