dfe1a738c605e0f8a2e5773150e78d352c58c2a2
Telmen17 <telmen7948@gmail.com>
2024-04-17 15:23:58 -0700

    Added Tester.java for testing Shark.java

---
+++ Tester.java
@@ -0,0 +1,14 @@
+/**
+ * A tester class for shark.java
+ * @author Telmen Enkhtuvshin
+ */
+public class Tester {
+    public static void main(String[] args){
+        Shark realShark = new Shark("King Shark");
+        realShark.eat();
+        realShark.play();
+        realShark.sound();
+        realShark.jaws();
+        realShark.swim();
+    }
+}
