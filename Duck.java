14501268cf955b2efeed0c8ac07e9b0aca00ca3c
Santos Gonzalez <gonzalezsan@cwu.edu>
2024-04-17 16:34:31 -0700

    added String name to Duck.java

---
+++ Duck.java
@@ -0,0 +1,20 @@
+//Santos Gonzalez
+//4/17/24
+
+
+/**
+ * Duck class represents a duck extending the Animal class.
+ */
+public class Duck extends Animal {
+
+    /**
+     * Constructor for Duck class.
+     * 
+     * @param name The name of the duck.
+     */
+    public Duck(String name) {
+        super(name);
+    }
+
+   }
+
