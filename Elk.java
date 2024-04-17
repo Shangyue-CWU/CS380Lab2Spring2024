63e179cc4a81e83e4ca5231cb4b897004f9cc6dc
Aaron Hodzic <hodzica@cwu.edu>
2024-04-16 23:02:12 -0700

    Added play() function

---
+++ Elk.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Elk extends Animal{
+
+   /**
+    * A constructor that takes in a name.
+    *@param name Name of the animal.
+    */
+   public Elk(String n){
+      super(n);
+   }
+
+
+   /**
+    * Abstract method that should represent a specific sound
+    * an animal makes.
+    */
+   public void sound(){
+      int soundDeterminant = (int)((Math.random() + 1) * 3);
+      if(soundDeterminant == 1){ System.out.println("Ghwraaheeeeeee"); }
+      else if(soundDeterminant == 2){ System.out.println("Brbrrreeeeeyew"); }
+      else { System.out.println("Grreap eeeereeeeeheeum"); }
+   }
+
+
+   /**
+    * Abstract method that should represent a specific  activity
+    * that an animal does for fun.
+    */
+   public void play(){
+      int soundDeterminant = (int)(Math.random() * 3) + 1;
+      if(soundDeterminant == 1){ System.out.println(super.name + " sloshed through a HUGE puddle."); }
+      else if(soundDeterminant == 2){ System.out.println("Overturned a small car."); }
+      else { System.out.println("Trampled a bear."); }
+   }
+
+
+   /**
+    * A  method that prints out that the animal is eating food.
+    */
+   public void eat(){
+      System.out.println(this.name + " eats some food.");
+   }
+}
+
