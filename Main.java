70e6f2879cdc83aa05f20dcaa8e0239bec6662f6
Yonatanym <166750160+Yonatanym@users.noreply.github.com>
2024-04-18 16:33:51 -0700

    Update Main.java
    add the main tester

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Raven raven1 = new Raven("Riri");
+        raven1.sound();
+        
+        raven1.play();
+        raven1.eat();
+    
+    }
+    
+}
