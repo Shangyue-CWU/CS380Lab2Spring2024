42500052aabe882a67b796a32be77620599a3d61
ChauAry21 <aryanachauhan21@gmail.com>
2024-04-17 20:41:42 -0700

    added javadoc comments

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
+/**
+ * This class is the main point of the program
+ */
 public class Main {
 
+    /**
+     * The main method of the program
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
+        // Create rabbit object named "Jimothy"
         Rabbit rabbit = new Rabbit("Jimothy");
+
+        // Make the rabbit produce sound
         rabbit.sound();
+
+        // Make the rabbit play
         rabbit.play();
+
+        // Make the rabbit eat
         rabbit.eat();
     }
-
 }
--- Rabbit.java
+++ Rabbit.java
@@ -1,20 +1,37 @@
+/**
+ * This class constructs a rabbit, of type Animal
+ */
 public class Rabbit extends Animal {
 
+    /**
+     * Constructs a new rabbit object with the given name.
+     * 
+     * @param name The name of the rabbit.
+     */
     public Rabbit(String name) {
         super(name);
         this.name = name;
     }
 
+    /**
+     * Produces a sound characteristic of a rabbit.
+     */
     @Override
     public void sound() {
         System.out.println(name + " squeaks!");
     }
 
+    /**
+     * Makes the rabbit play.
+     */
     @Override
     public void play() {
         System.out.println(name + " jumps!");
     }
 
+    /**
+     * Makes the rabbit eat grass.
+     */
     public void eat() {
         System.out.println(name + " eats grass!");
     }
