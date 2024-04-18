12894c5af4aa96643ede8dbab5e756f4d27099c8
ArwenOC <arwenatormc@gmail.com>
2024-04-17 23:28:03 -0700

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+
+   /*
+   * Tests animal and eagle class
+   * @param args arguments of function
+   */
+   public static void main(String[] args) {
+   
+      Eagle eagle0 = new Eagle("Eagleton");
+      
+      eagle0.sound();
+      eagle0.play();
+      eagle0.eat();
+   
+   }
+}
